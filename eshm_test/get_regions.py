a7eef92a16cea7fd5934f0cf4047f2b449b9f4c7
pciturri <pciturri@gfz-potsdam.de>
2023-11-16 12:50:10 +0100

    sty: rearranged run code

--- eshm_test/get_regions.py
+++ eshm_test/get_regions.py
@@ -1,11 +1,10 @@
 import os
-from os.path import join, normpath, dirname
-import oq2csep
+from os.path import join, dirname
 from oq2csep.cmd import main
-from oq2csep.region_lib import intersect_region
-import numpy as np
+
 
 dir_script = dirname(__file__)
+
 eshm13_sm = join(dir_script, 'eshm13', 'SHARE_OQ_input_20140807')
 ESHM13_PATH = {'seifa': join(eshm13_sm, 'seifa_model.xml'),
                'fsbg': join(eshm13_sm, 'faults_backg_source_model.xml'),
@@ -14,7 +13,6 @@ ESHM13_PATH = {'seifa': join(eshm13_sm, 'seifa_model.xml'),
 
 eshm20_sm = join(dir_script, 'eshm20', 'oq_computational',
                  'oq_configuration_eshm20_v12e_region_main/source_models')
-
 eshm20_subd_interface = [join(eshm20_sm, 'interface_v12b', i)
                          for i in ['CaA_IF2222222_M40.xml',
                                    'CyA_IF2222222_M40.xml',
@@ -22,9 +20,9 @@ eshm20_subd_interface = [join(eshm20_sm, 'interface_v12b', i)
                                    'HeA_IF2222222_M40.xml']]
 ESHM20_PATH = {
     'as': [join(eshm20_sm, 'asm_v12e',
-                           'asm_ver12e_winGT_fs017_lo_abgrs_maxmag_low.xml'),
+                'asm_ver12e_winGT_fs017_lo_abgrs_maxmag_low.xml'),
            join(eshm20_sm, 'asm_v12e',
-                           'asm_ver12e_winGT_fs017_twingr.xml'),
+                'asm_ver12e_winGT_fs017_twingr.xml'),
            *eshm20_subd_interface],
     'fsbg': [
            join(eshm20_sm, 'fsm_v09', 'fs_ver09e_model_aGR_SRL_ML_fMthr.xml'),
@@ -43,18 +41,15 @@ if __name__ == '__main__':
     reg_eshm13_seifa = join(output_dir, 'region_eshm13_seifa.txt')
     reg_eshm13 = join(output_dir, 'region_eshm2013.txt')
 
-
     main.region(ESHM13_PATH['as'], dest=reg_eshm13_as, plot=True)
     main.region(ESHM13_PATH['fsbg'], dest=reg_eshm13_fsbg, plot=True)
     main.region(ESHM13_PATH['seifa'], dest=reg_eshm13_seifa, plot=True)
     main.region([reg_eshm13_as, reg_eshm13_fsbg, reg_eshm13_seifa],
                 dest=reg_eshm13, intersect=True, plot=True)
 
-
     reg_eshm20_as = join(output_dir, 'region_eshm20_as.txt')
     reg_eshm20_fsbg = join(output_dir, 'region_eshm20_fsbg.txt')
     reg_eshm20 = join(output_dir, 'region_eshm20.txt')
-
     main.region(ESHM20_PATH['as'], dest=reg_eshm20_as, plot=True)
     main.region(ESHM20_PATH['fsbg'], dest=reg_eshm20_fsbg, plot=True)
     main.region([reg_eshm20_as, reg_eshm20_fsbg],
