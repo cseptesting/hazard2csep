b8c441973bdf06a8ff866befbf8cc512f8bddc3f
pciturri <pciturri@gfz-potsdam.de>
2023-12-03 16:50:36 +0100

    fix to add proj path to libraries

--- hazard2csep/__init__.py
+++ hazard2csep/__init__.py
@@ -1,7 +1,20 @@
+import os
+
 from hazard2csep import region_lib
 from hazard2csep import forecast_lib
 from hazard2csep import sm_lib
 from hazard2csep import logger
+
+# Handle proj installation issue to point bin libraries
+try:
+    venv = os.environ['CONDA_PREFIX']
+    proj_path = os.path.join(venv, 'share')
+
+except KeyError:  # using python-venv
+    venv = os.environ['VIRTUAL_ENV']
+    proj_path = os.path.join(venv, 'share')
+
+os.environ['PROJ_LIB'] = f'{proj_path}/proj'
 
 
 __version__ = '0.1.1'
